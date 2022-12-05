e868a424728e47c06db32c38439731ecb08b80e6
Josue-07 <josuelima875@gmail.com>
2022-12-05 00:15:21 -0300

    first commit

---
+++ src/test/java/br/com/josuelima/apitest/APITest.java
@@ -0,0 +1,70 @@
+package br.com.josuelima.apitest;
+
+import static io.restassured.RestAssured.*;
+
+import org.apache.http.HttpStatus;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.http.ContentType;
+
+
+public class APITest {
+	
+	@BeforeAll
+	public static void setup() {
+		baseURI = "http://localhost:8001/tasks-backend";
+				
+		
+	}
+
+	@Test
+	public void deveRetornarTarefas() {
+		given()
+			.log().all()
+		
+		.when()
+			.get("/todo")
+				
+			.then()
+			.log().all()
+			.statusCode(HttpStatus.SC_OK)
+			;
+	}
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		given()
+		.log().all()
+		.contentType(ContentType.JSON)
+		.body("{\"task\":\"Test API\",\r\n"
+				+ "\"dueDate\":\"2023-12-15\"}")
+		
+		.when()
+			.post("/todo")
+		
+		.then()
+			.log().all()
+			.statusCode(HttpStatus.SC_CREATED)
+			;
+	}
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		given()
+		.log().all()
+		.contentType(ContentType.JSON)
+		.body("{\"task\":\"Test API\",\r\n"
+				+ "		\"dueDate\":\"2020-12-15\"}")
+		
+		.when()
+		.post("/todo")
+		
+		.then()
+		.log().all()
+		.statusCode(HttpStatus.SC_BAD_REQUEST)
+		.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+	
+
+}
